8a4186ca38bece256d99b851211658d6231e2236
Johann Cohen-Tanugi <johann.cohen-tanugi@umontpellier.fr>
2022-11-15 17:42:58 +0100

    fix setup.py and put the cython file in src dir

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules = cythonize(["funcs.pyx"]),
+    ext_modules = cythonize(["src/funcs.pyx"]),
     include_dirs=[numpy.get_include()]
 )
